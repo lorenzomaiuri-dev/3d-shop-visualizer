446af336dc8a7659cdc0548795d95014691a8779
lorenzomaiuri-dev <maiurilorenzo@gmail.com>
2026-03-14 13:28:47 +0100

    feat: added component for showing model from db in scene

---
+++ src/features/configurator/components/ConfigurableModel.tsx
@@ -0,0 +1,28 @@
+import { useGLTF } from '@react-three/drei'
+import { useConfiguratorStore } from '../../../store/useConfiguratorStore'
+import { useEffect } from 'react'
+import * as THREE from 'three'
+
+export function ConfigurableModel() {
+  const { product, selectedVariant } = useConfiguratorStore()
+
+  const { scene } = useGLTF(product?.model_path || '')
+
+  useEffect(() => {
+    if (!scene || !selectedVariant) return
+
+    scene.traverse((child: THREE.Mesh) => {
+      if (child instanceof THREE.Mesh) {
+        console.log('Mesh:', child.name)
+        if (child.name === selectedVariant.target_mesh) {
+          child.material.color.set(selectedVariant.color)
+        }
+
+        child.castShadow = true
+        child.receiveShadow = true
+      }
+    })
+  }, [scene, selectedVariant])
+
+  return <primitive object={scene} />
+}
